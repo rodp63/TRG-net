Fix grpm scaling

sample.py
@@ -2,21 +2,26 @@ import time
 
 import cv2
 import torch
+import torchvision
 import torchvision.transforms as transforms
+
 from imutils.video import VideoStream
 from PIL import Image
 
 from trgnet.zoo import trgnet_mobilenet_v3_large
+from trgnet.data import Kitti
 
 video = True
 use_grpm = True
-model = trgnet_mobilenet_v3_large(pretrained=True, grpm_min_area=20, grpm_lr=0.01)
+grpm_show_output = True
 
+model = trgnet_mobilenet_v3_large(
+    pretrained=True, grpm_min_area=50, grpm_lr=0.01, grpm_show_output=grpm_show_output
+)
 model.eval()
-# print(sum(p.numel() for p in model.parameters() if p.requires_grad))
 
 if video:
-    cap = cv2.VideoCapture("data/vtest.mp4")
+    cap = cv2.VideoCapture("data/test.mp4")
 else:
     cap = VideoStream(src=0).start()
 
@@ -34,11 +39,14 @@ with torch.no_grad():
         pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         pil_to_tensor = transforms.ToTensor()(pil_img).unsqueeze_(0)
 
+        a = time.time()
         pred = model(pil_to_tensor, use_grpm=use_grpm, grpm_image=frame)
+        b = time.time()
 
         for idx, b in enumerate(pred[0]["boxes"]):
-            if pred[0]["scores"][idx] < 0.5:
+            if pred[0]["scores"][idx] < 0.4:
                 continue
+            print(Kitti.classes[pred[0]["labels"][idx]], pred[0]["scores"][idx])
             x1, y1, x2, y2 = b[0].item(), b[1].item(), b[2].item(), b[3].item()
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
@@ -49,7 +57,6 @@ with torch.no_grad():
         if k == 27:
             break
 
-    model.timer.report()
-
+    # model.timer.report()
     cap.release()
     cv2.destroyAllWindows()

trgnet/grpm.py
@@ -6,15 +6,17 @@ import torch.nn as nn
 
 
 class GaussianRegionProposal(nn.Module):
-    def __init__(self, min_area=20, lr=0.01):
+    def __init__(self, min_area, lr, show_output):
         super().__init__()
         self.min_area = min_area
         self.lr = lr
+        self.show = show_output
         self.gmm = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
         self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
-    def forward(self, image):
+    def forward(self, image, new_size):
         boxes = []
+        image = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
         mask = self.gmm.apply(image, learningRate=self.lr)
         mask[mask < 255] = 0
 
@@ -22,12 +24,25 @@ class GaussianRegionProposal(nn.Module):
         contours, hierarchy = cv2.findContours(
             img_close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
         )
+
         for cnt in contours:
             if cv2.contourArea(cnt) >= self.min_area:
                 x, y, w, h = cv2.boundingRect(cnt)
                 boxes.append(torch.Tensor([x, y, x + w, y + h]))
+                for t in range(1, 3):
+                    boxes.append(torch.Tensor([x - t, y - t, x + w + t, y + h + t]))
+                    boxes.append(
+                        torch.Tensor(
+                            [x - t - 2, y - t - 2, x + w + t + 2, y + h + t + 2]
+                        )
+                    )
+                if self.show:
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         if len(boxes) == 0:
             boxes = [torch.Tensor([0, 0, 1, 1])]
+        if self.show:
+            cv2.imshow("grpm", image)
+            cv2.imshow("mask", mask)
 
         return [torch.stack(boxes)]

trgnet/training/utils.py
@@ -8,7 +8,7 @@ def load_training(model, optimizer=None, epoch=7, name="trgnet"):
     pt_path = os.path.join(user_path, f".trgnet/checkpoints/{name}-{epoch}.pt")
     checkpoint = torch.load(pt_path, map_location=torch.device("cpu"))
 
-    model.load_state_dict(checkpoint["model_state_dict"])
+    model.load_state_dict(checkpoint["model_state_dict"], strict=False)
     if optimizer is not None:
         optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 

trgnet/trg.py
@@ -45,7 +45,8 @@ class GeneralizedTRG(nn.Module):
 
         self.timer.start("RPM")
         if use_grpm:
-            proposals = self.grpm(grpm_image)
+            new_size = (images.tensors.shape[3], images.tensors.shape[2])
+            proposals = self.grpm(grpm_image, new_size)
             proposal_losses = {}
         else:
             proposals, proposal_losses = self.rpn(images, features, targets)
@@ -134,6 +135,7 @@ class TRGNet(GeneralizedTRG):
         # GRPM
         grpm_min_area=20,
         grpm_lr=-1,
+        grpm_show_output=False,
     ):
         out_channels = backbone.out_channels
 
@@ -201,6 +203,6 @@ class TRGNet(GeneralizedTRG):
             image_std = [0.229, 0.224, 0.225]
         transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std)
 
-        grpm = GaussianRegionProposal(grpm_min_area, grpm_lr)
+        grpm = GaussianRegionProposal(grpm_min_area, grpm_lr, grpm_show_output)
 
         super().__init__(backbone, rpn, roi_heads, transform, grpm)